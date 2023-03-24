b8393c25cf1400f88fdc9500db17b89cfd6e60fb
Edwin Li <eli212@student.fuhsd.org>
2023-03-24 11:49:23 -0700

    javadoc + fix some errors

--- PriceComparator.java
+++ PriceComparator.java
@@ -1,42 +1,52 @@
 /**
  * A price comparator for trade orders.
+ * 
+ * @author Edwin Li
+ * @author William Li
+ * @version 3/24/2023
  */
-public class PriceComparator implements java.util.Comparator<TradeOrder>
+public class PriceComparator
+    implements java.util.Comparator<TradeOrder>
 {
     private boolean ascending;
 
     /**
-     * Constructs a price comparator that compares two orders in ascending order.
+     * Constructs a price comparator that compares two orders in ascending
+     * order.
      */
     public PriceComparator()
     {
         ascending = true;
     }
 
+
     /**
-     * Constructs a price comparator that compares two orders in 
-     * ascending or descending order. The order of comparison depends
-     * on the value of a given parameter. Sets the private boolean ascending flag to asc.
+     * Constructs a price comparator that compares two orders in ascending or
+     * descending order. The order of comparison depends on the value of a given
+     * parameter. Sets the private boolean ascending flag to asc.
+     * 
      * @param asc
      */
-    public PriceComparator(boolean asc) //asc = ascending
+    public PriceComparator(boolean asc) // asc = ascending
     {
         ascending = asc;
     }
 
+
     /**
      * Compares two trade orders.
-     * @param order1 - the first order
-     * @param order2 - the second order
-     * @return 0 if both orders are market orders;
-     *         -1 if order1 is market and order2 is limit;
-     *         1 if order1 is limit and order2 is market;
-     *         the difference in prices, rounded to the nearest cent, if both 
-     * order1 and order2 are limit orders. In the latter case, 
-     * the difference returned is cents1 - cents2 or cents2 - cents1, 
-     * depending on whether this is an ascending or 
-     * descending comparator (ascending is true or false).
      * 
+     * @param order1
+     *            - the first order
+     * @param order2
+     *            - the second order
+     * @return 0 if both orders are market orders; -1 if order1 is market and
+     *         order2 is limit; 1 if order1 is limit and order2 is market; the
+     *         difference in prices, rounded to the nearest cent, if both order1
+     *         and order2 are limit orders. In the latter case, the difference
+     *         returned is cents1 - cents2 or cents2 - cents1, depending on
+     *         whether this is an ascending or descending comparator (ascending
+     *         is true or false).
      */
     public int compare(TradeOrder order1, TradeOrder order2)
     {
@@ -55,16 +65,16 @@ public class PriceComparator implements java.util.Comparator<TradeOrder>
             return 1;
         }
 
-        else //if (order1.isLimit() && order2.isLimit())
+        else // if (order1.isLimit() && order2.isLimit())
         {
             if (ascending)
             {
-                int x = (int) Math.round((order1.getPrice()*100 - order2.getPrice()*100));
+                int x = (int)Math.round((order1.getPrice() * 100 - order2.getPrice() * 100));
                 return x;
             }
             else
             {
-                int x = (int) Math.round((order2.getPrice()*100 - order1.getPrice()*100));
+                int x = (int)Math.round((order2.getPrice() * 100 - order1.getPrice() * 100));
                 return x;
             }
         }
--- TradeOrder.java
+++ TradeOrder.java
@@ -3,28 +3,45 @@ import java.lang.reflect.*;
 /**
  * Represents a buy or sell order for trading a given number of shares of a
  * specified stock.
+ * 
+ * @author Edwin Li
+ * @author William Li
+ * @version 3/24/2023
  */
 public class TradeOrder
 {
-    private Trader trader;
-    private String symbol;
+    private Trader  trader;
+    private String  symbol;
     private boolean buyOrder;
     private boolean marketOrder;
-    private int numShares;
-    private double price;
+    private int     numShares;
+    private double  price;
 
     /**
-     * Constructs a new TradeOrder for a given trader,
-     * stock symbol, a number of shares, and other parameters.
+     * Constructs a new TradeOrder for a given trader, stock symbol, a number of
+     * shares, and other parameters.
      * 
-     * @param trader - a trader who placed this order.
-     * @param symbol - stock symbol.
-     * @param buyOrder - if true this is a buy order; otherwise this is a sell order.
-     * @param marketOrder - if true this is a market order; otherwise this is a limit order.
-     * @param numShares - the number of shares to be traded.
-     * @param price - the bid or ask price, if this is a limit order.
+     * @param trader
+     *            - a trader who placed this order.
+     * @param symbol
+     *            - stock symbol.
+     * @param buyOrder
+     *            - if true this is a buy order; otherwise this is a sell order.
+     * @param marketOrder
+     *            - if true this is a market order; otherwise this is a limit
+     *            order.
+     * @param numShares
+     *            - the number of shares to be traded.
+     * @param price
+     *            - the bid or ask price, if this is a limit order.
      */
-    public TradeOrder (Trader trader, String symbol, boolean buyOrder, boolean marketOrder, int numShares, double price)
+    public TradeOrder(
+        Trader trader,
+        String symbol,
+        boolean buyOrder,
+        boolean marketOrder,
+        int numShares,
+        double price)
     {
         this.trader = trader;
         this.symbol = symbol;
@@ -33,67 +50,77 @@ public class TradeOrder
         this.numShares = numShares;
         this.price = price;
     }
-    
+
+
     /**
      * Returns the price per share for this trade order (used by a limit order).
      * 
      * @return the price per share for this trade order.
      */
-    public double getPrice() 
+    public double getPrice()
     {
         return price;
     }
+
 
     /**
      * Returns the number of shares to be traded in this trade order.
      * 
      * @return the number of shares to be traded in this trade order.
      */
-    public int getShares() 
+    public int getShares()
     {
         return numShares;
     }
+
 
     /**
      * Returns the stock symbol for this trade order.
      * 
      * @return the stock symbol for this trade order.
      */
-    public String getSymbol() 
+    public String getSymbol()
     {
         return symbol;
     }
+
 
     /**
      * Returns true if this is a buy order; otherwise returns false.
      * 
      * @return true if this is a buy order; false otherwise.
      */
-    public Trader getTrader() 
+    public Trader getTrader()
     {
         return trader;
     }
 
+
     /**
      * Returns true if this is a buy order; otherwise returns false.
+     * 
      * @return true if this is a buy order; false otherwise.
      */
-    public boolean isBuy() 
+    public boolean isBuy()
     {
         return buyOrder;
     }
 
+
     /**
      * Returns true if this is a limit order; otherwise returns false.
+     * 
      * @return true if this is a limit order; false otherwise.
      */
-    public boolean isLimit() 
+    public boolean isLimit()
     {
         return !marketOrder;
     }
 
+
     /**
      * Returns true if this is a market order; otherwise returns false.
+     * 
      * @return true if this is a market order; false otherwise.
      */
     public boolean isMarket()
@@ -101,8 +128,10 @@ public class TradeOrder
         return marketOrder;
     }
 
+
     /**
      * Returns true if this is a sell order; otherwise returns false.
+     * 
      * @return true if this is a sell order; false otherwise.
      */
     public boolean isSell()
@@ -110,23 +139,31 @@ public class TradeOrder
         return !buyOrder;
     }
 
+
     /**
-     * Subtracts a given number of shares from the total number of shares in this trade order.
-     * @param shares - a number of shares to be subtracted.
-     * @throws java.lang.IllegalArgumentException - if shares is greater than the number of shares in this order.
+     * Subtracts a given number of shares from the total number of shares in
+     * this trade order.
+     * 
+     * @param shares
+     *            - a number of shares to be subtracted.
+     * @throws java.lang.IllegalArgumentException
+     *             - if shares is greater than the number of shares in this
+     *             order.
      */
     public void subtractShares(int shares)
     {
-        if (!(numShares > shares))
+        if (numShares >= shares)
         {
             numShares -= shares;
             return;
         }
         else
         {
-            throw new IllegalArgumentException("shares are greater than the number of shares in this order");
+            throw new IllegalArgumentException(
+                "shares are greater than the number of shares in this order");
         }
     }
+
 
     //
     // The following are for test purposes only
@@ -147,16 +184,16 @@ public class TradeOrder
 
         Field[] fields = this.getClass().getDeclaredFields();
 
-        for ( Field field : fields )
+        for (Field field : fields)
         {
             try
             {
-                str += separator + field.getType().getName() + " "
-                    + field.getName() + ":" + field.get( this );
+                str += separator + field.getType().getName() + " " + field.getName() + ":"
+                    + field.get(this);
             }
-            catch ( IllegalAccessException ex )
+            catch (IllegalAccessException ex)
             {
-                System.out.println( ex );
+                System.out.println(ex);
             }
 
             separator = ", ";
